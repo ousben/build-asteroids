5a8e36b580fbbffaae25673451f957fd0d3c0e14
Oussama Ben Guirat <benguiratoussama1998@gmail.com>
2024-11-20 14:02:20 +0100

    initialization of the game environment

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+
+def main():
+    # initialize the game engine
+    pygame.init()
+    print("Starting asteroids!")
+
+if __name__ == "__main__":
+    main()
