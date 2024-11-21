7047b75db8d1269ada9bab8c43f1a241d93b4a10
Oussama Ben Guirat <benguiratoussama1998@gmail.com>
2024-11-21 11:01:05 +0100

    game time features

--- main-folder/main.py
+++ main-folder/main.py
@@ -9,6 +9,9 @@ def main():
     pygame.init()
     print("Starting asteroids!")
 
+    clock = pygame.time.Clock() 
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -18,6 +21,8 @@ def main():
         screen.fill((0, 0, 0))
         # Rafraîchir l'affichage
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
