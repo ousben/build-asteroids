b28d2132af1fe8f76db1370bfdbfbe327667af75
Oussama Ben Guirat <benguiratoussama1998@gmail.com>
2024-11-21 13:24:04 +0100

    creation of the Player main character

--- main-folder/main.py
+++ main-folder/main.py
@@ -1,30 +1,35 @@
 from constants import *
+from player import Player
 import pygame
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 
 def main():
     # initialize the game engine
     pygame.init()
     print("Starting asteroids!")
 
+    # Créer la fenêtre et le joueur à l'intérieur de main()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     clock = pygame.time.Clock() 
     dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
                 return
-    
+
         # Remplir l'écran en noir
         screen.fill((0, 0, 0))
-        # Rafraîchir l'affichage
-        pygame.display.flip()
         
-        dt = clock.tick(60) / 1000
+        # Dessiner le joueur avant le flip
+        player.draw(screen)
+        
+        # Rafraîchir l'affichage en dernier
+        pygame.display.flip()
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
